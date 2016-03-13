6310384c99d90e311e5b7094581330ffbb2ddbfd
Leo is awesome <lchan1994@yahoo.com>
2016-03-13 11:59:42 -0400

    added rest of app code

--- app/src/com/example/autoregister/MainActivity.java
+++ app/src/com/example/autoregister/MainActivity.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
 import android.util.Log;
+import android.view.View;
 
 import java.lang.Runnable;
 import java.lang.Thread;
@@ -15,16 +21,59 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+
+import org.json.JSONArray;
 
 public class MainActivity extends Activity implements Runnable {
-	private int n;
+	/* This is hardocded for now and must change depending on where the server is running. */
+	private String ENDPOINT = "http://192.168.1.110:8080/test_post";
+
+	private int n;  // This auto-incrementing variable is just for testing that the app will still run while asleep
 	private TextView tv;
 	private Handler handler;
+	private Button submit;
+	private DatePicker datePicker;
+	private TimePicker timePicker;
+	private long timeTicket;
+	private EditText drexelID, password, crns;
+	private boolean sent = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        /* Setup */
+        drexelID = (EditText) findViewById(R.id.drexelID);
+        password = (EditText) findViewById(R.id.password);
+        crns = (EditText) findViewById(R.id.crns);
+        datePicker = (DatePicker) findViewById(R.id.date_picker);
+        timePicker = (TimePicker) findViewById(R.id.time_picker);
+        timeTicket = Long.MAX_VALUE;
+
+        submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View view){
+				Calendar calendar = new GregorianCalendar(
+										datePicker.getYear(),
+						                datePicker.getMonth(),
+						                datePicker.getDayOfMonth(),
+						                timePicker.getCurrentHour(),
+						                timePicker.getCurrentMinute());
+				timeTicket = calendar.getTimeInMillis();
+				String datetime = String.format("Set new time ticket of %d%d%d at %d:%d",
+										datePicker.getYear(),
+						                datePicker.getMonth(),
+						                datePicker.getDayOfMonth(),
+						                timePicker.getCurrentHour(),
+						                timePicker.getCurrentMinute());
+				sent = false;
+				Toast.makeText(getApplicationContext(), datetime, Toast.LENGTH_SHORT).show();
+        	}
+        });
 
         handler = new Handler();
         n = 0;
@@ -34,7 +83,7 @@ public class MainActivity extends Activity implements Runnable {
 
 	@Override                    
 	public void run() {       
-		tv = (TextView) findViewById(R.id.tv);    
+		// tv = (TextView) findViewById(R.id.tv);
 		while (true) {
 	        try {
 	        	// Asynchronously update the GUI on a thread separate from the
@@ -43,8 +92,18 @@ public class MainActivity extends Activity implements Runnable {
 	        	handler.post(new Runnable(){
 	        		@Override
 	        		public void run(){
-	        			tv.setText(n + "");
-	        			post();
+	        			// tv.setText(n + "");
+	        			if (!sent && System.currentTimeMillis() > timeTicket){
+	        				String id = drexelID.getText().toString();
+	        				String psswd = password.getText().toString();
+	        				JSONArray crnsJSON = new JSONArray();
+	        				for (String crn : crns.getText().toString().split(",")){
+	        					crnsJSON.put(crn);
+	        				}
+
+	        				post(id, psswd, crnsJSON.toString());
+	        				sent = true;
+	        			}
 	        		}
 	        	});
 	            Thread.sleep(1000);
@@ -73,16 +132,16 @@ public class MainActivity extends Activity implements Runnable {
         return builder.toString();
     }
 
-	private void post(){
+	private void post(String id, String password, String crnsJSON){
 		try {
-	        URL url = new URL("http://192.168.1.110:8080/test_post");
+	        URL url = new URL(ENDPOINT);
 	        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
 	        httpCon.setDoOutput(true);
 	        httpCon.setRequestMethod("POST");
 	        httpCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 	        httpCon.setRequestProperty("charset", "utf-8");
 	        OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
-	        String urlParameters  = "param1=a&param2=b";
+	        String urlParameters = String.format("id=%s&password=%s&crns=%s", id, password, crnsJSON);
 	        out.write(urlParameters);
 	        out.close();
 	        Log.v("TAG", handleStream(httpCon.getInputStream()));
--- test_server/app.py
+++ test_server/app.py
@@ -8,6 +8,7 @@ Test server to receive requests from the android app.
 
 from __future__ import print_function
 
+import json
 
 from flask import Flask, request, jsonify
 from uuid import uuid4
@@ -19,15 +20,18 @@ app.secret_key = str(uuid4())
 @app.route("/test_post", methods=["POST"])
 def test_post():
     """Route for receiving post data."""
-    param1 = request.form.get("param1", None)
-    param2 = request.form.get("param2", None)
+    param1 = request.form.get("id", None)
+    param2 = request.form.get("password", None)
+    param3 = request.form.get("crns", None)
 
     print("param1: ", param1)
     print("param2: ", param2)
+    print("param3: ", param3, json.loads(param3))
 
     return jsonify(
         param1=param1 or "None",
-        param2=param2 or "None"
+        param2=param2 or "None",
+        param3=param3 or "None"
     )
 
 
